dd4e442aa1fafb5ba8d48777830af2a1e107dace
SaktiID <51077353+saktiID@users.noreply.github.com>
2025-02-05 14:56:25 +0700

    tambah fitur fullscreen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 const chatToggle = document.querySelector(".chat-toggle");
+const chatFullscreenBtn = document.querySelector(".fullscreen-button");
 const chatHideBtn = document.querySelector(".hide-button");
 const chatContainer = document.querySelector(".chat-container");
 const chatInputText = document.querySelector(".chat-input-text");
@@ -32,7 +33,7 @@ const generateResponse = async (incomingMessageDiv) => {
       contents: userData.chatHistory,
       system_instruction: {
         parts: {
-          text: "Anda adalah Assistane, nama Anda adalah Zick. Anda dikembangkan oleh Romosakti dengan url instagram https://www.instagram.com/ach.m4d22/ di kota Sidoarjo dan diberdayakan oleh Gemini dari Google. Anda mengetahui informasi tentang Romosakti, Romosakti adalah pengembang Anda dan bisa dikenal atau diketahui lebih lanjut dengan mengunjungi link instagram yang sudah diberikan.",
+          text: "Anda adalah Assistane, nama Anda adalah Zick. Anda dikembangkan oleh Romosakti dengan url instagram https://www.instagram.com/ach.m4d22/ di Sidoarjo. Anda mengetahui informasi tentang Romosakti, Romosakti adalah pengembang Anda dan bisa dikenal atau diketahui lebih lanjut dengan mengunjungi link instagram yang sudah diberikan.",
         },
       },
     }),
@@ -143,4 +144,13 @@ chatToggle.addEventListener("click", () => {
 
 chatHideBtn.addEventListener("click", () => {
   chatContainer.classList.remove("show-chat");
+});
+
+chatFullscreenBtn.addEventListener("click", () => {
+  chatContainer.classList.toggle("fullscreen");
+  if (chatFullscreenBtn.classList.toggle("active")) {
+    chatFullscreenBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M456 224l-144 0c-13.3 0-24-10.7-24-24l0-144c0-9.7 5.8-18.5 14.8-22.2s19.3-1.7 26.2 5.2l40 40L442.3 5.7C446 2 450.9 0 456 0s10 2 13.7 5.7l36.7 36.7C510 46 512 50.9 512 56s-2 10-5.7 13.7L433 143l40 40c6.9 6.9 8.9 17.2 5.2 26.2s-12.5 14.8-22.2 14.8zm0 64c9.7 0 18.5 5.8 22.2 14.8s1.7 19.3-5.2 26.2l-40 40 73.4 73.4c3.6 3.6 5.7 8.5 5.7 13.7s-2 10-5.7 13.7l-36.7 36.7C466 510 461.1 512 456 512s-10-2-13.7-5.7L369 433l-40 40c-6.9 6.9-17.2 8.9-26.2 5.2s-14.8-12.5-14.8-22.2l0-144c0-13.3 10.7-24 24-24l144 0zm-256 0c13.3 0 24 10.7 24 24l0 144c0 9.7-5.8 18.5-14.8 22.2s-19.3 1.7-26.2-5.2l-40-40L69.7 506.3C66 510 61.1 512 56 512s-10-2-13.7-5.7L5.7 469.7C2 466 0 461.1 0 456s2-10 5.7-13.7L79 369 39 329c-6.9-6.9-8.9-17.2-5.2-26.2s12.5-14.8 22.2-14.8l144 0zM56 224c-9.7 0-18.5-5.8-22.2-14.8s-1.7-19.3 5.2-26.2l40-40L5.7 69.7C2 66 0 61.1 0 56s2-10 5.7-13.7L42.3 5.7C46 2 50.9 0 56 0s10 2 13.7 5.7L143 79l40-40c6.9-6.9 17.2-8.9 26.2-5.2s14.8 12.5 14.8 22.2l0 144c0 13.3-10.7 24-24 24L56 224z"/></svg>`;
+  } else {
+    chatFullscreenBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M200 32L56 32C42.7 32 32 42.7 32 56l0 144c0 9.7 5.8 18.5 14.8 22.2s19.3 1.7 26.2-5.2l40-40 79 79-79 79L73 295c-6.9-6.9-17.2-8.9-26.2-5.2S32 302.3 32 312l0 144c0 13.3 10.7 24 24 24l144 0c9.7 0 18.5-5.8 22.2-14.8s1.7-19.3-5.2-26.2l-40-40 79-79 79 79-40 40c-6.9 6.9-8.9 17.2-5.2 26.2s12.5 14.8 22.2 14.8l144 0c13.3 0 24-10.7 24-24l0-144c0-9.7-5.8-18.5-14.8-22.2s-19.3-1.7-26.2 5.2l-40 40-79-79 79-79 40 40c6.9 6.9 17.2 8.9 26.2 5.2s14.8-12.5 14.8-22.2l0-144c0-13.3-10.7-24-24-24L312 32c-9.7 0-18.5 5.8-22.2 14.8s-1.7 19.3 5.2 26.2l40 40-79 79-79-79 40-40c6.9-6.9 8.9-17.2 5.2-26.2S209.7 32 200 32z"/></svg>`;
+  }
 });
